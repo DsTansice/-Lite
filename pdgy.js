06eaf5d634e7755b3004dbacd90b7dce7244d1e7
dstansice <246009756@qq.com>
2023-02-05 20:40:45 +0800

    Site updated: 2023-02-05 20:40:44

--- pdgy.js
+++
@@ -1,1 +0,0 @@
-var foreignTips=function(){var o=function(o){if(o.country.iso_code){o.country.iso_code.toLowerCase();"cn"&&btf.snackbarShow("If you use Chinese network access, you will not be able to access articles and pictures.")}},n=function(o){};return function(){geoip2.country(o,n)}}();foreignTips();
